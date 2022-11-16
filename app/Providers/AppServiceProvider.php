f05b730ec9cc7119d8ad081df94a6435b05b7786
gagan.pasupuleti <gagan.pasupuleti@techigai.io>
2022-11-16 12:59:17 +0530

    Quick fix for routing CSS files to https & added docker file for base image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\App;
+use URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -27,5 +30,8 @@ class AppServiceProvider extends ServiceProvider
         // if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
         //     $this->app['request']->server->set('HTTPS', true);
         // }
+        
+        if (App:: environment(['staging', 'production'])) {
+            URL:: forceScheme('https');
     }
 }
